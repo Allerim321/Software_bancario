0474c6038b12ce959fc9369a4b32bb41fc21c6a9
Mirella <mirellatcg@gmail.com>
2023-08-10 15:58:16 -0300

    match case

--- Algoritmo.py
+++ Algoritmo.py
@@ -3,6 +3,7 @@ import os
 def main():
     while True:
         try:
+            
             print("Bem Vindo ao PazBank")
             print("[1] - Cadastrar cliente")
             print("[2] - Excluir cliente")
@@ -12,23 +13,23 @@ def main():
             print("[6] - Sair")
 
             op = int(input("Digite o número equivalente à opção que deseja:\n"))
-
-            if op == 1:
-                print("")
-            elif op == 2:
-                print("")
-            elif op == 3:
-                print("")
-            elif op == 4:
-                print("")
-            elif op == 5:
-                print("")
-            elif op == 6: 
-                print("Saindo...")
-                os.system("pause")
-                exit()
-            else:
-                print("Essa opção não consta na lista.")
+            match op:
+                case 1:
+                    print("")
+                case 2:
+                    print("")
+                case 3:
+                    print("")
+                case 4:
+                    print("")
+                case 5:
+                    print("")
+                case 6: 
+                    print("Saindo...")
+                    os.system("pause")
+                    exit()
+                case _:
+                    print("Essa opção não consta na lista.")
         except Exception as erro:
             print("Algo deu errado. Tente novamente.")
             print(erro.__class__.__name__)
