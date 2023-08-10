478e4db3cc51617d5053f132aecc34bc1a6ff13d
diegorkkj <diegosenaisp@gmail.com>
2023-08-10 14:43:36 -0300

    upload inicio do menu

---
+++ Algoritmo.py
@@ -0,0 +1,9 @@
+import os
+
+def main():
+    while True:
+        print("Bem Vindo ao PazBank")
+        print("[1] - ")
+        print("[2] - ")
+        print("[3] - ")
+        print("[4] - Sair")
