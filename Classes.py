4aaa9d453f349f6a96b37e84555c099dd334f42c
nat-paz <nataliapazsa@gmail.com>
2023-08-17 13:22:19 -0300

    up

--- Classes.py
+++ Classes.py
@@ -1,16 +1,16 @@
 import random
 class Cliente:
 
-    def sacar(self, valor):
-        if valor > self.saldo:
+    def sacar(self, valor,saldo):
+        if valor > saldo:
             print("Não é possível sacar esse valor")
         else:
-            self.saldo -= valor
-            print("Valor sacado com sucesso! Seu saldo agora é de R$", self.saldo)
+            saldo -= valor
+            print("Valor sacado com sucesso! Seu saldo agora é de R$", saldo)
 
-    def depositar(self, valor):
-        self.saldo += valor
-        print("Valor depositado com sucesso! Seu saldo agora é de R$", self.saldo)
+    def depositar(self, valor, saldo):
+        saldo += valor
+        print("Valor depositado com sucesso! Seu saldo agora é de R$", saldo)
 
     def transferencia(self, num_conta1, num_conta2, valor):
         num_conta1.saldo=self.saldo - valor
@@ -19,12 +19,20 @@ class Cliente:
 
 class Banco:
 
-    def adicionar_conta():
-        num_conta = random.randint(1000,9999)
-        nome = ""
-        idade = 0
-        cpf = 0
-        saldo = 0
+    def adicionar_conta(self):
+        self.num_conta = random.randint(1000,9999)
+        self.nome = ""
+        self.idade = 0
+        self.cpf = 0
+        self.saldo = 0
+
+    def getNome(self):
+        return self.nome
+    
+    def setNome(self,x):
+        self.nome = x
+        
+
 
 
 clientes = []
