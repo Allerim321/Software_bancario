0e10ff16ecf95921e052ddd918d4e8b090dfbfe7
pablo11-dev <pablofr2018@gmail.com>
2023-08-17 08:08:12 -0300

    comit1 Pablo

--- Classes.py
+++ Classes.py
@@ -1,18 +1,35 @@
 class Cliente:
 
     def __init__(self, nome, cpf, idade, saldo):
-        self.nome=nome
-        self.cpf=cpf
-        self.idade=idade
-        self.saldo=saldo
+        self.nome = nome
+        self.cpf = cpf
+        self.idade = idade
+        self.saldo = saldo
 
-    def sacar(self,valor):
+    def sacar(self, valor):
         if valor > self.saldo:
             print("Não é possível sacar esse valor")
         else:
-            self.saldo = self.saldo - valor
-            print("Valor sacado com sucesso! Seu saldo agora é de R$",self.saldo)
+            self.saldo -= valor
+            print("Valor sacado com sucesso! Seu saldo agora é de R$", self.saldo)
 
-    def depositar(self,valor):
-            self.saldo = self.saldo + valor
-            print("Valor depositado com sucesso! Seu saldo agora é de R$",self.saldo)
+    def depositar(self, valor):
+        self.saldo += valor
+        print("Valor depositado com sucesso! Seu saldo agora é de R$", self.saldo)
+
+
+class Conta:
+
+    def __init__(self, saldo, cliente):
+        self.saldo = saldo
+        self.cliente = cliente
+
+
+class Banco:
+
+    def __init__(self):
+        self.contas = []
+
+    def adicionar_conta(self, saldo, cliente):
+        conta = Conta(saldo, cliente)
+        self.contas.append(conta)
